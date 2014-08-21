60162eb86b3ecf15514e2b4589db50a295b43b24
Alex <alexsofia359@gmail.com>
2014-08-21 18:20:19 +0300

    First

---
+++ src/AlbumTdg/Controller/AlbumTdgController.php
@@ -0,0 +1,90 @@
+<?php
+namespace AlbumTdg\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+use Zend\Db\TableGateway\TableGateway;
+
+use AlbumTdg\Form\AlbumForm;
+use AlbumTdg\Form\AlbumFilter;
+
+class AlbumTdgController extends AbstractActionController
+{
+	private $albumTable = null;
+	
+	//R- Retrieve
+	public function indexAction()
+	{
+		// 1) Identity
+		// $user = $this->identity();
+		// \Doctrine\Common\Util\Debug::dump($user);
+		// 2) SQL trough adapter
+		// $adapter = $this->getAlbumTable()->getAdapter();
+		// $result = $adapter->query('SELECT * FROM `album` WHERE `id` = ?', array(2));
+		// print_r($result);
+		return new ViewModel(array('rowset' => $this->getAlbumTable()->select()));
+	}
+	//C- Create
+	public function createAction()
+	{
+		$form = new AlbumForm();
+		$request = $this->getRequest();	
+        if ($request->isPost()) {
+			$form->setInputFilter(new AlbumFilter());
+			$form->setData($request->getPost());
+			 if ($form->isValid()) {
+				$data = $form->getData();
+				unset($data['submit']);
+				$this->getAlbumTable()->insert($data);
+				return $this->redirect()->toRoute('album_tdg/default', array('controller' => 'album-tdg', 'action' => 'index'));										
+			}
+		}		
+		return new ViewModel(array('form' => $form));
+	}
+	//U- Update
+	public function updateAction()
+	{
+		$id = $this->params()->fromRoute('id');
+		if (!$id) return $this->redirect()->toRoute('album_tdg/default', array('controller' => 'album-tdg', 'action' => 'index'));
+		$form = new AlbumForm();
+		$request = $this->getRequest();
+		if ($request->isPost()) {
+			$form->setInputFilter(new AlbumFilter());
+			$form->setData($request->getPost());
+			 if ($form->isValid()) {
+				$data = $form->getData();
+				unset($data['submit']);
+				$this->getAlbumTable()->update($data, array('id' => $id));
+				return $this->redirect()->toRoute('album_tdg/default', array('controller' => 'album-tdg', 'action' => 'index'));													
+			}				 
+		}
+		else {
+			$form->setData($this->getAlbumTable()->select(array('id' => $id))->current());			
+		}
+		return new ViewModel(array('form' => $form, 'id' => $id));
+	}
+	//D- Delete
+	public function deleteAction()
+	{
+		$id = $this->params()->fromRoute('id');
+		if ($id) {
+			$this->getAlbumTable()->delete(array('id' => $id));
+		}
+
+		return $this->redirect()->toRoute('album_tdg/default', array('controller' => 'album-tdg', 'action' => 'index'));	
+	}
+	public function getAlbumTable()
+	{
+		if (!$this->albumTable) {
+			// $this->albumTable = new Album($this->getServiceLocator()->get('Zend\Db\Adapter\Adapter'));
+			$this->albumTable = new TableGateway( // Album( // 
+				'album', 
+				$this->getServiceLocator()->get('Zend\Db\Adapter\Adapter')
+//				new \Zend\Db\TableGateway\Feature\RowGatewayFeature('usr_id') // Zend\Db\RowGateway\RowGateway Object
+//				ResultSetPrototype
+			);
+		}
+		return $this->albumTable;
+	}
+}
